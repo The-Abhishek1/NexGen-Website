f03d5b8365125630d1c32b125e13e05eab00893b
The-Abhishek1 <idiot63666@gmail.com>
2024-03-08 15:39:11 +0530

    Made Conatct window responsive

--- src/Components/FAQSlide/FAQSlide.jsx
+++ src/Components/FAQSlide/FAQSlide.jsx
@@ -12,72 +12,72 @@ export default function FAQSlide() {
       <h1 className={f.h1}>FAQ</h1>
       <div className={f.items}>
         <div className={f.subItems} onClick={() => setFirst(!first)}>
-          <h3 className={f.h3}>How are paid plans billed?</h3>
+          <h3 className={f.h3}>
+            What types of websites can NexGen Coders help me build?
+          </h3>
           {first ? (
             <>
               <div className={f.content}>
                 <p className={f.p}>
-                  Paid plans are billed both at a Site and Team level. Both
-                  subscriptions are always linked to the email of the purchaser.
-                  In order to get extra features and faster loading time to your
-                  site, you can acquire any of our site plans. In the moment of
-                  purchasing the first site plan within a team, a Team account
-                  will be added automatically so editors will be charged at a
-                  team level.
+                  NexGen Coders can assist in building various types of
+                  websites, including business websites, e-commerce platforms,
+                  blogs, portfolios, and more. Just let us know your
+                  requirements, and we'll tailor a solution to suit your needs.
                 </p>
               </div>
             </>
           ) : null}
         </div>
         <div className={f.subItems} onClick={() => setSecond(!second)}>
-          <h3 className={f.h3}>How are paid plans billed?</h3>
+          <h3 className={f.h3}>
+            {" "}
+            How often will my website be updated and maintained?
+          </h3>
           {second ? (
             <>
               <div className={f.content}>
                 <p className={f.p}>
-                  Paid plans are billed both at a Site and Team level. Both
-                  subscriptions are always linked to the email of the purchaser.
-                  In order to get extra features and faster loading time to your
-                  site, you can acquire any of our site plans. In the moment of
-                  purchasing the first site plan within a team, a Team account
-                  will be added automatically so editors will be charged at a
-                  team level.
+                  We offer flexible maintenance plans based on your specific
+                  needs. Whether you require weekly, monthly, or on-demand
+                  updates, our team ensures your website remains current with
+                  the latest content, security patches, and performance
+                  optimizations.
                 </p>
               </div>
             </>
           ) : null}
         </div>{" "}
         <div className={f.subItems} onClick={() => setThird(!third)}>
-          <h3 className={f.h3}>How are paid plans billed?</h3>
+          <h3 className={f.h3}>
+            Can NexGen Coders develop mobile apps for both iOS and Android
+            platforms?
+          </h3>
           {third ? (
             <>
               <div className={f.content}>
                 <p className={f.p}>
-                  Paid plans are billed both at a Site and Team level. Both
-                  subscriptions are always linked to the email of the purchaser.
-                  In order to get extra features and faster loading time to your
-                  site, you can acquire any of our site plans. In the moment of
-                  purchasing the first site plan within a team, a Team account
-                  will be added automatically so editors will be charged at a
-                  team level.
+                  Absolutely! Our expertise extends to both iOS and Android app
+                  development. Whether you need an app for one platform or
+                  cross-platform compatibility, we have the skills and
+                  experience to bring your mobile app vision to life.
                 </p>
               </div>
             </>
           ) : null}
         </div>{" "}
         <div className={f.subItems} onClick={() => setFourth(!fourth)}>
-          <h3 className={f.h3}>How are paid plans billed?</h3>
+          <h3 className={f.h3}>
+            How long does it take to develop a mobile app with NexGen Coders?
+          </h3>
           {fourth ? (
             <>
               <div className={f.content}>
                 <p className={f.p}>
-                  Paid plans are billed both at a Site and Team level. Both
-                  subscriptions are always linked to the email of the purchaser.
-                  In order to get extra features and faster loading time to your
-                  site, you can acquire any of our site plans. In the moment of
-                  purchasing the first site plan within a team, a Team account
-                  will be added automatically so editors will be charged at a
-                  team level.
+                  The timeline for app development varies depending on the
+                  complexity of your project and specific requirements. However,
+                  we strive to deliver high-quality mobile apps within a
+                  reasonable timeframe, ensuring timely delivery without
+                  compromising on quality.
                 </p>
               </div>
             </>
